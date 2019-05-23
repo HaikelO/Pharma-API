f14a26bcc7202f61670302451f615c370bb1f860
HaikelO <haikel.ben@gmail.com>
2019-05-23 15:57:13 +0200

    Adding Post & Delete Drug

--- src/routes/controllers/drug/index.ts
+++ src/routes/controllers/drug/index.ts
@@ -1,20 +1,55 @@
-"use strict"
+"use strict";
 
-import express from "express"
-import { Drug, DrugDocument } from "./../../../models/Drug"
-import { Request, Response, NextFunction } from "express"
+import express from "express";
+import {Drug, DrugDocument} from "./../../../models/Drug";
+import {Request, Response, NextFunction} from "express";
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const id: string = req.params.id
-    const drugs: Array<DrugDocument> = await Drug.find({})
-    console.log("result_get_drug :", drugs)
-    res.send(id)
+    const id: string = req.params.id;
+    const drugs: Array<DrugDocument> = await Drug.find({});
+    console.log("result_get_drug :", drugs);
+    res.send(id);
   } catch (error) {
-    console.log("Error Get Drug Find ALL", error)
+    console.log("Error Get Drug Find ALL", error);
   }
-})
+});
 
-export default router
+router.post("/", async (req: any, res: Response, next: NextFunction) => {
+  try {
+    const drug = new Drug({
+      name: req.body.name
+    });
+    const isDrug: Array<DrugDocument> = await Drug.find({name: drug.name});
+    if (isDrug.length > 0) {
+      console.log("drug_already_exist");
+      res.status(200).send({message: "drug_already_exist"});
+    } else {
+      drug.save((err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send({message: err});
+        } else {
+          console.log(result);
+          res.status(201).send(result);
+        }
+      });
+    }
+  } catch (error) {
+    console.log("Error Post Drug ", error);
+  }
+
+  router.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const _id = req.params.id
+      const removeDrug =  await Drug.deleteOne({_id})
+      console.log("removeDrug", removeDrug)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+});
+
+export default router;
